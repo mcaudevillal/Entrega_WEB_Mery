7fda4cd3be4f99ee22ebb5204b41cb931f2b9d06
Maria Caudevilla Lamban <mariacaudevillalam@gmail.com>
2019-06-18 19:15:49 +0200

    entrega

---
+++ visor_leaflet/js/visor_leaflet.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">  
+  
+  <title>Mapa básico con Leaflet</title>  
+  <link rel="stylesheet" href="E:\Visualizacion_online_Caudevilla_Maria\leaflet\visor_leaflet\css\leaflet.css"/>
+  <script src="E:\Visualizacion_online_Caudevilla_Maria\leaflet\visor_leaflet\js\leaflet.js"></script>
+</head>
+<body>
+
+  <script src="E:\Visualizacion_online_Caudevilla_Maria\leaflet\visor_leaflet\js\leaflet.js"></script>
+  var mapa = new L.map('mi-mapa', {
+    center: [20, 50],
+    zoom: 2    
+});
+</body>
+</html>
